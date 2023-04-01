3a845bb2608cfa7d49f3de0afc6871be26d150da
Sushil Gharti Magar <64299496+sushilghartimgr@users.noreply.github.com>
2023-04-01 09:45:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,411 @@
+
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>window.location.href ='index.php'</script>";
+
+} ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Hospital management System </title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+     <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+    <body>
+
+    <!-- ################# Header Starts Here#######################--->
+    
+      <header id="menu-jk">
+    
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="margin-top: 1% !important;">
+                       <a href="../index.php" class="logo me-auto me-lg-0"><img src="logo.png" style="height: 80px;" alt="" class="img-fluid"></a>
+                    </div>
+                    <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#about">About Us</a></li>
+                            <li><a href="#gallery">Gallery</a></li>
+                            <li><a href="#contact_us">Contact Us</a></li>
+                            <li><a href="#logins">Logins</a></li>  
+                        </ul>
+                    </div>
+                    <div class="col-sm-2 d-none d-lg-block appoint">
+                        <a class="btn btn-success" href="hms/user-login.php">Book an Appointment</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    
+     <!-- ################# Slider Starts Here#######################--->
+
+    <div class="slider-detail">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+   
+
+
+            <div class="carousel-inner">
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="assets/images/slider/slider_2.png" alt="Second slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Life Care</h5>
+            
+                         
+                    
+                    </div>
+                </div>
+                
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/slider/slider_3.png" alt="Third slide">
+                      <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Welcome to Life Care Hospital</h5>
+            
+                         
+                    
+                    </div>
+              
+                </div>
+                
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </div>
+    
+  <!--  ************************* Logins ************************** -->
+    
+    
+     <section id="logins" class="our-blog container-fluid">
+        <div class="container">
+        <div class="inner-title">
+
+                <h2>Logins</h2>
+            </div>
+            <div class="col-sm-12 blog-cont">
+                <div class="row no-margin">
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="patients.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Patient Login</h6>
+                                <a href="hms/user-login.php" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="doctors.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Doctors login</h6>
+                                <a href="hms/doctor" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="admins.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Admin Login</h6>
+                    
+                                <a href="hms/admin" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+
+                    
+                    
+                </div>
+            </div>
+            
+        </div>
+    </section>  
+
+
+
+
+
+
+
+    <!-- ################# Our Departments Starts Here#######################--->
+
+
+    <section id="services" class="key-features department">
+        <div class="container">
+            <div class="inner-title">
+
+                <h2>Our Key Features</h2>
+                <p>Take a look at some of our key features</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-heartbeat"></i>
+                        <h5>Cardiology</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-ribbon"></i>
+                        <h5>Orthopaedic</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                       <i class="fab fa-monero"></i>
+                        <h5>Neurologist</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-capsules"></i>
+                        <h5>Pharma Pipeline</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-prescription-bottle-alt"></i>
+                        <h5>Pharma Team</h5>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="far fa-thumbs-up"></i>
+                        <h5>High Quality treatments</h5>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+    
+    
+  
+   
+     <!-- ======= About Section ======= -->
+     <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100">
+            <div class="about-img">
+              <img src="doctors.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>About Us</h3>
+            <p class="fst-italic">
+              Life Care Hospital is a modern medical facility committed to providing exceptional healthcare services to our patients. Our experienced and compassionate staff use the latest technologies to deliver accurate diagnoses and effective treatments in a safe and comfortable environment. Thank you for choosing Life Care Hospital for your healthcare needs.
+            </p>
+<!--             <ul>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+            </ul>
+             -->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Gallery Section ======= -->
+    <section id="gallery" class="gallery">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>Gallery</h2>
+          <p>Some photos from our Hospital</p>
+        </div>
+
+        <div class="row g-0">
+
+          <div class="col-lg-3 col-md-4">
+            <div class="galjpglery-item">
+              <a href="doctor.jpg" class="gallery-lightbox">
+                <img src="doctors.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="admin.jpg" class="gallery-lightbox">
+                <img src="admins.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="patient.jpg" class="gallery-lightbox">
+                <img src="patients.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="doctor.jpg" class="gallery-lightbox">
+                <img src="doctors.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="admin.jpg" class="gallery-lightbox">
+                <img src="admins.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="patient.jpg" class="gallery-lightbox">
+                <img src="patients.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="doctor.jpg" class="gallery-lightbox">
+                <img src="doctors.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Gallery Section -->
+
+    
+     <!--  ************************* Contact Us Starts Here ************************** -->
+    
+    <section id="contact_us" class="contact-us-single">
+        <div class="row no-margin">
+
+            <div  class="col-sm-12 cop-ck">
+                <form method="post">
+                <h2 >Contact Form</h2>
+                    <div class="row cf-ro">
+                        <div  class="col-sm-3"><label>Enter Name :</label></div>
+                        <div class="col-sm-8"><input type="text" placeholder="Enter Name" name="fullname" class="form-control input-sm" required ></div>
+                    </div>
+                    <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>Email Address :</label></div>
+                        <div class="col-sm-8"><input type="text" name="emailid" placeholder="Enter Email Address" class="form-control input-sm"  required></div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>Mobile Number:</label></div>
+                        <div class="col-sm-8"><input type="text" name="mobileno" placeholder="Enter Mobile Number" class="form-control input-sm" required ></div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>Enter  Message:</label></div>
+                        <div class="col-sm-8">
+                          <textarea rows="5" placeholder="Enter Your Message" class="form-control input-sm" name="description" required></textarea>
+                        </div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label></label></div>
+                        <div class="col-sm-8">
+                         <button class="btn btn-success btn-sm" type="submit" name="submit">Send Message</button>
+                        </div>
+                </div>
+            </form>
+            </div>
+     
+        </div>
+    </section>
+    
+    
+    
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+    <div class="container">
+      <h3>Life Care - Hospital</h3>
+      <p>We care about your Life </p>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Life Care - Hospital</span></strong>. All Rights Reserved
+      </div></div></footer>
+
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/jquery.easing.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/scrolling-nav.js"></script>
+<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+
+</body>
+</html>
